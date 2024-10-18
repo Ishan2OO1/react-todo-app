ff13e4412cfc3ea1c12409e26f165a8a3f535473
Ishan2001 <ishanchaudhary2001@gmail.com>
2024-10-18 16:51:00 -0700

    first code push

---
+++ client/my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <ul>
+        <li>1. Rest</li>
+        <li>2. Walk</li>
+        <li>3. Sleep</li>
+        <li>4. Eat</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
